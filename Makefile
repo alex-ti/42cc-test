504d9b3e3912712f9ecb74b25b875c5b694fb727
MotoMoto <moto.moto@ukr.net>
2014-02-10 15:10:35 +0200

    ticket#1

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ MANAGE=django-admin.py
 
 test:
 	PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=test42cc.settings $(MANAGE) test test42cc
+	PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=test42cc.settings $(MANAGE) test person
 
 run:
 	PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=test42cc.settings $(MANAGE) runserver
--- person/tests.py
+++ person/tests.py
@@ -1,3 +1,4 @@
+# -*- coding: utf8 -*-
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
@@ -5,8 +6,11 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from django.core.urlresolvers import reverse
 from django.test import TestCase
-
+from django.test.client import Client
+from person.models import Person
+from datetime import date
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +18,26 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+class HttpTest(TestCase):
+    def setUp(self):
+        self.person = Person.objects.create(
+            firstname = u'Firstname',
+            lastname = u'Lastname',
+            birthday = date.today(),
+            bio = u'biodata',
+            email = u'email@example.com',
+            jabber = u'jabber@example.com',
+            skype = u'skype_login',
+            other = u'Other contact data'
+        )
+
+    def test_personView(self):
+        c = Client()
+        response = c.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '42 Coffee Cups Test Assignment')
+        self.assertContains(response, self.person.firstname)
+        self.assertContains(response, self.person.lastname)
+        self.assertContains(response, self.person.email)
